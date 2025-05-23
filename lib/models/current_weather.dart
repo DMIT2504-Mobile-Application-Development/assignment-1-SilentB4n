897b40794975eef197125869af0bfa7ec047b36f
josh <your.email@fakee.com>
2025-05-22 21:25:52 -0600

    update

---
+++ lib/models/current_weather.dart
@@ -0,0 +1,9 @@
+
+class CurrentWeather {
+  late String _city;
+  late String _description;
+}
+
+
+
+
--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -8,11 +8,17 @@ const currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather'
 
 Future<dynamic> getWeatherForCity({required String city}) async {
 
-    final uri = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
-    final response = await http.get(uri);
-
-    if (response.statusCode != 200) {
-      throw Exception('There was a problem with the request: status ${response.statusCode} received');
-    }
+     try{  
+    var uri = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
+    
+    var response =  await http.get(uri);
+    
+    if (response.statusCode != 200 ){
+        throw Exception("There was a problem with the request: status ${response.statusCode} received.");
+    } 
     return jsonDecode(response.body);
+
+    } catch (e){
+    throw Exception("there was a problem with the request: '$e'");
+    }
 }
