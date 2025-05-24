4017274667c5b7c987834d152a7df424562d20f6
josh <your.email@fakee.com>
2025-05-23 20:18:45 -0600

    update

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -1,8 +1,29 @@
 
 class CurrentWeather {
-  late String _city;
-  late String _description;
+   late String _city;
+    late String _description;
+    late double _currentTemp;
+    late DateTime _currentTime;
+    late DateTime _sunrise;
+    late DateTime _sunset;
+
+    CurrentWeather({
+    required String city, 
+    required String description,
+    required double currentTemp,
+    required DateTime currentTime,
+    required DateTime sunrise,
+    required DateTime sunset,})
+   {  _city = city;
+      _description = description;
+      _currentTemp = currentTemp;
+      _currentTime = currentTime;
+      _sunrise = sunrise;
+      _sunset = sunset;
+    }
 }
+
+
 
 
 
